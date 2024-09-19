44eae06befbdce843851569700b4222da3a7e8b7
sharath kannan <shkannan@adobe.com>
2024-09-19 13:35:12 +0530

    Acessibilty feature added to videos

--- libs/blocks/video/video.js
+++ libs/blocks/video/video.js
@@ -2,7 +2,20 @@ import { createIntersectionObserver, getConfig } from '../../utils/utils.js';
 import { applyHoverPlay, getVideoAttrs, applyInViewPortPlay } from '../../utils/decorate.js';
 
 const ROOT_MARGIN = 1000;
-
+const handlePause = (event) => {
+  const video = event.target.parentElement.parentElement.querySelector('video');
+  const playIcon = event.target.closest('.play-icon') || event.target.querySelector('.play-icon');
+  const pauseIcon = event.target.closest('.pause-icon') || event.target.querySelector('.pause-icon');
+  if (video.paused) {
+    video.play();
+    pauseIcon.classList.remove('hidden');
+    playIcon.classList.add('hidden')
+  } else {
+    video.pause();
+    playIcon.classList.remove('hidden');
+    pauseIcon.classList.add('hidden');
+  }
+}
 const loadVideo = (a) => {
   const { pathname, hash, dataset } = a;
   let videoPath = `.${pathname}`;
@@ -16,12 +29,24 @@ const loadVideo = (a) => {
   }
 
   const attrs = getVideoAttrs(hash, dataset);
-  const video = `<video ${attrs}>
+  const video = `<div><video ${attrs}>
         <source src="${videoPath}" type="video/mp4" />
-      </video>`;
+      </video>
+      <div class='pause-play-wrapper' tabindex=0>
+        <img class='pause-icon' src='/drafts/sharathkannan/accessiblity/pause-video.svg'/>
+        <img class='play-icon hidden' src='/drafts/sharathkannan/accessiblity/play-video.svg'/>
+      </div>
+      <div>`;
+  // https://main--cc--adobecom.hlx.page/drafts/sharathkannan/accessiblity/play-video.svg
+  // https://main--cc--adobecom.hlx.page/drafts/sharathkannan/accessiblity/pause-video.svg
+  // https://main--cc--adobecom.hlx.page/cc-shared/assets/img/device-icons/devices-icon.svg
   if (!a.parentNode) return;
   a.insertAdjacentHTML('afterend', video);
   const videoElem = document.body.querySelector(`source[src="${videoPath}"]`)?.parentElement;
+  const pausePlayWrapper = videoElem.parentElement.querySelector('.pause-play-wrapper');
+  pausePlayWrapper.addEventListener('click', handlePause);
+  pausePlayWrapper.addEventListener('keydown', handlePause)
+
   applyHoverPlay(videoElem);
   applyInViewPortPlay(videoElem);
   a.remove();
